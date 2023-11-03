09964223d1d5fbb7816ac9dec30147eb112f4d63
Yuyi Li <72236124+Yuy1L1@users.noreply.github.com>
2023-11-02 19:11:23 -0700

    Update startup.c, added setColorPalette function

--- riscv-example/src/startup.c
+++ riscv-example/src/startup.c
@@ -1,5 +1,5 @@
 #include <stdint.h>
-
+#include <stdio.h>
 extern uint8_t _erodata[];
 extern uint8_t _data[];
 extern uint8_t _edata[];
@@ -37,6 +37,32 @@ __attribute__((always_inline)) inline void csr_disable_interrupts(void){
 #define MTIMECMP_HIGH   (*((volatile uint32_t *)0x40000014))
 #define CONTROLLER      (*((volatile uint32_t *)0x40000018))
 
+#define INTERRUPT_ENABLE_REGISTEER 0x40000000
+#define INTERRUPT_PENDING_REGISTEER 0x40000004
+#define CATRIDGE_STATUS_REGISTER 0x4000001C
+
+#define SMALL_SPRITE_DATA_ADDR 0x500E0000
+#define MEDIUM_SPRITE_DATA_ADDR 0x500D0000
+#define LARGE_SPRITE_DATA_ADDR 0x50090000
+
+#define SMALL_SPRITE_SIZE 0x100  // data size of 256B as specified in video.md
+#define MEDIUM_SPRITE_SIZE 0x400 // data size of 1KiB as specified in video.md
+#define LARGE_SPRITE_SIZE 0x1000 // data size of 4KiB as specified in video.md
+
+#define SMALL_SPRITE_CONTROL_ADDR 0x500F6300
+#define MEDIUM_SPRITE_CONTROL_ADDR 0x500F5F00
+#define LARGE_SPRITE_CONTROL_ADDR 0x500F5B00
+
+#define SMALL_SPRITE_CONTROL_SIZE 0x4 
+#define MEDIUM_SPRITE_CONTROL_SIZE 0x4 
+#define LARGE_SPRITE_CONTROL_SIZE 0x4
+
+#define MODE_CONTROL_REGISTER 0x500FF414
+#define GRAPHICS_MEMORY 0x50000000
+
+#define SMALL_SPRITE_PALETTE_ADDR 0x500F3000
+// todo define medium and large sprite palette address
+
 void init(void){
     uint8_t *Source = _erodata;
     uint8_t *Base = _data < _sdata ? _data : _sdata;
@@ -69,3 +95,82 @@ void c_interrupt_handler(void){
     controller_status = CONTROLLER;
 }
 
+uint32_t small_sprite_counter = 0;
+uint32_t medium_sprite_counter = 0;
+uint32_t large_sprite_counter = 0;
+
+int setGraphicMode(){
+    // default mode: 0, text mode
+    uint32_t curr_mode = *((volatile uint32_t *) MODE_CONTROL_REGISTER);
+    // printf("%x", curr_mode);
+    curr_mode |=1;
+    return 0;
+}
+
+int setColorPalette(uint32_t palette_number, uint32_t color, uint32_t entry_number){
+    // each palette is of 1 KiB size 1 KiB = 1024 B = 0x400
+    // each entry is worth 4 B -> 0x4
+    // color is for the RGB val, entry_number is for which entry in the palette to change
+    uint32_t * palette = (volatile uint32_t *) (SMALL_SPRITE_PALETTE_ADDR + (0x400) * palette_number + (0x4) *entry_number);  
+    palette[0] = color;
+    return 0;
+}
+
+// drawSprite(Sprite sprite, int x, int y, int z)
+uint16_t drawSprite(uint32_t sprite_control_structure){
+
+    uint32_t x, y, z, width, height, palette;
+    palette = sprite_control_structure & 0b11;
+    x = (((1 << 10) - 1) & (sprite_control_structure >> 10));
+    y = (((1 << 9) - 1) & (sprite_control_structure >> 19));
+    z = (((1 << 3) - 1) & (sprite_control_structure >> 22));
+    width = x - 16;
+    height = y - 16;
+
+    // check if sprite is large, medium or small
+    // if (width < 16 && height < 16){
+        // dynamically draw small sprite 
+    uint8_t * small_data_addr = (volatile uint8_t * )(SMALL_SPRITE_DATA_ADDR + small_sprite_counter * SMALL_SPRITE_SIZE);
+    for (int i = 0; i < 16; i++){
+        for (int j = 0; j < 16; j++){
+            // if there is color, need to color within bounds of width and height
+            small_data_addr[i * 64 + j] = 0; 
+            // TODO: color can be customized in the future
+        }
+    }
+
+    uint32_t * small_control_addr = (volatile uint32_t *)(SMALL_SPRITE_CONTROL_ADDR + small_sprite_counter * SMALL_SPRITE_CONTROL_SIZE);
+    small_control_addr[0] = sprite_control_structure;
+    small_sprite_counter ++;
+    // }
+    // // medium sprite
+    // if (width < 64 && height < 64){
+    //     uint32_t * medium_control_addr = (volatile uint32_t *)(MEDIUM_SPRITE_CONTROL_ADDR + medium_sprite_counter * MEDIUM_SPRITE_CONTROL_SIZE);
+    //     medium_control_addr[0] = sprite_control_structure;
+    //     medium_control_addr ++;
+    // }
+    // // a large sprite
+    // if (width > 64 && height > 64)
+    // {
+    //     uint32_t * large_control_addr = (volatile uint32_t *)(LARGE_SPRITE_CONTROL_ADDR + large_sprite_counter * LARGE_SPRITE_CONTROL_SIZE);
+    //     large_control_addr[0] = sprite_control_structure;
+    //     large_sprite_counter ++;
+    // }
+    // // successful
+    return 0;
+}
+
+
+void cmd_interrupt(){
+    uint32_t interrupt_pending_register = *((volatile uint32_t * ) INTERRUPT_PENDING_REGISTEER);
+    interrupt_pending_register = interrupt_pending_register & 0x00000003;
+}
+
+volatile int video_counter = 0;
+
+void video_interrupt(){
+    video_counter += 1;
+    uint32_t interrupt_pending_register = *((volatile uint32_t * ) INTERRUPT_PENDING_REGISTEER);
+    // need to write 1 to the video bit in IPR, which is the second bit.
+    interrupt_pending_register = interrupt_pending_register & 0x00000002;
+}
