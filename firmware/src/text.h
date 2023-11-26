07b3fe29b3694e5b0f8ce1cf9909de32bc932a2d
Yuyi Li <72236124+Yuy1L1@users.noreply.github.com>
2023-11-26 00:58:00 -0800

    Update text.h

--- firmware/src/text.h
+++ firmware/src/text.h
@@ -1,8 +1,10 @@
 #include <stdlib.h>
-#include <stdio.h>
-#include <stdbool.h>
+#include <stdint.h>
+#include <string.h>
 
-#define TEXT_PALETTE_ADDR 0x500F6700
+#define TEXT_PALETTE_ADDR     0x500F6700
+#define MODE_CONTROL_REGISTER 0x500F6780
+#define GRAPHICS_MEMORY       0x50000000
 
 int setTextMode();
 void setTextColorPalette(uint16_t entry_number, uint32_t background_color, uint32_t foreground_color);
