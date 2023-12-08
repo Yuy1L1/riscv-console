1b5c7a52e3016b3e370d900eb9cb80e710ecc759
Yuyi Li <72236124+Yuy1L1@users.noreply.github.com>
2023-12-07 20:38:04 -0800

    Add files via upload

---
+++ group_11/CMakeFiles/cartridge.dir/DependInfo.cmake
@@ -0,0 +1,38 @@
+
+# Consider dependencies only in project.
+set(CMAKE_DEPENDS_IN_PROJECT_ONLY OFF)
+
+# The set of languages for which implicit dependencies are needed:
+set(CMAKE_DEPENDS_LANGUAGES
+  "ASM"
+  )
+# The set of files for implicit dependencies of each language:
+set(CMAKE_DEPENDS_CHECK_ASM
+  "/code/riscv-render/cartridge/src/crt0.s" "/code/riscv-render/cartridge/CMakeFiles/cartridge.dir/src/crt0.s.o"
+  "/code/riscv-render/cartridge/src/interrupt.s" "/code/riscv-render/cartridge/CMakeFiles/cartridge.dir/src/interrupt.s.o"
+  )
+set(CMAKE_ASM_COMPILER_ID "GNU")
+
+# Preprocessor definitions for this target.
+set(CMAKE_TARGET_DEFINITIONS_ASM
+  "RISCV_CONSOLE"
+  )
+
+# The include file search paths:
+set(CMAKE_ASM_TARGET_INCLUDE_PATH
+  "cartridge/PureC-Software-Renderer/Mango/src"
+  )
+
+# The set of dependency files which are needed:
+set(CMAKE_DEPENDS_DEPENDENCY_FILES
+  "/code/riscv-render/cartridge/src/cube.c" "cartridge/CMakeFiles/cartridge.dir/src/cube.c.o" "gcc" "cartridge/CMakeFiles/cartridge.dir/src/cube.c.o.d"
+  "/code/riscv-render/cartridge/src/main.c" "cartridge/CMakeFiles/cartridge.dir/src/main.c.o" "gcc" "cartridge/CMakeFiles/cartridge.dir/src/main.c.o.d"
+  )
+
+# Targets to which this target links.
+set(CMAKE_TARGET_LINKED_INFO_FILES
+  "/code/riscv-render/cartridge/PureC-Software-Renderer/Mango/CMakeFiles/Mango.dir/DependInfo.cmake"
+  )
+
+# Fortran module output directory.
+set(CMAKE_Fortran_TARGET_MODULE_DIR "")
