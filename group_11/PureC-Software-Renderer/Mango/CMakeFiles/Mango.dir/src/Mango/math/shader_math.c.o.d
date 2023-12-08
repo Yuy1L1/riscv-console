d1931a7bd7f52d9914850e811b0e8549fe04a672
Yuyi Li <72236124+Yuy1L1@users.noreply.github.com>
2023-12-07 20:53:19 -0800

    Delete group_11/PureC-Software-Renderer directory

--- group_11/PureC-Software-Renderer/Mango/CMakeFiles/Mango.dir/src/Mango/math/shader_math.c.o.d
+++
@@ -1,3 +0,0 @@
-cartridge/PureC-Software-Renderer/Mango/CMakeFiles/Mango.dir/src/Mango/math/shader_math.c.o: \
- /code/riscv-render/cartridge/PureC-Software-Renderer/Mango/src/Mango/math/shader_math.c \
- /code/riscv-render/cartridge/PureC-Software-Renderer/Mango/src/Mango/math/shader_math.h
